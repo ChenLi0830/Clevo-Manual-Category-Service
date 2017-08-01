e01c7bb09808510a45a59e28ca9257e67d200e02
Chen Li <yichen.li0830@gmail.com>
2017-07-31 21:19:25 -0700

    Update operator table when submitSpeech

--- serverless/graphql/database/speechSubmit.js
+++ serverless/graphql/database/speechSubmit.js
@@ -2,6 +2,8 @@ const sentenceCreate = require('./CRUD/sentenceCreate');
 const speechCreate = require('./CRUD/speechCreate');
 const rawSpeechGet = require('./CRUD/rawSpeechGet');
 const rawSpeechUpdate = require('./CRUD/rawSpeechUpdate');
+const operatorGet = require('./CRUD/operatorGet');
+const operatorUpdate = require('./CRUD/operatorUpdate');
 
 //fileName, operatorId, needReverseSpeaker, sentenceList
 const speechSubmit = (args) => {
@@ -12,12 +14,22 @@ const speechSubmit = (args) => {
     promises.push(sentenceCreate(sentence))
   });
   
-  // update uncategorizedSpeech
-  return rawSpeechGet(args.fileName)
-      .then(rawSpeech => {
-        let newFields = {categorizedCount: ~~rawSpeech.categorizedCount + 1};
-        return rawSpeechUpdate(args.fileName, newFields, {});
-      })
+  // update rawSpeechTable and operatorTable
+  return Promise.all([
+    rawSpeechGet(args.fileName)
+        .then(rawSpeech => {
+          let newFields = {categorizedCount: ~~rawSpeech.categorizedCount + 1};
+          return rawSpeechUpdate(args.fileName, newFields, {});
+        }),
+    operatorGet(args.operatorId)
+        .then(operator => {
+          let newFields = {
+            speechCount: ~~operator.speechCount + 1,
+            sentenceCount: ~~operator.sentenceCount + args.sentenceList.length,
+          };
+          return operatorUpdate(args.fileName, newFields, {});
+        })
+  ])
   // create speech and sentences
       .then(() => Promise.all(promises))
       .then(result => result[0])
