1a1f990de1a4bec83273a7c593c760ab71492ead
Chen Li <yichen.li0830@gmail.com>
2017-09-12 15:57:34 -0700

    update ProcessedSpeechType

--- serverless/graphql/types/ProcessedSpeechType.js
+++ serverless/graphql/types/ProcessedSpeechType.js
@@ -12,14 +12,40 @@ const graphql = require('graphql'),
 
 const EmotionType = require('./EmotionType');
 
+
+
+const CategorizedTranscriptType = new GraphQLObjectType({
+  name: "categorizedTranscript",
+  fields: () => ({
+    // categoryName: {type: GraphQLString},
+    // fileNameBeginTime: {type: GraphQLString},
+    // operatorId: {type: GraphQLID},
+    // fileName: {type: GraphQLString},
+    bg: {type: GraphQLString},
+    ed: {type: GraphQLString},
+    onebest: {type: GraphQLString},
+    speaker: {type: GraphQLString},
+    alertWords: {type: new GraphQLList(GraphQLString)},
+    bannedWords: {type: new GraphQLList(GraphQLString)},
+    categories: {type: new GraphQLList(GraphQLString)},
+  })
+});
+
 const ProcessedSpeechType = new GraphQLObjectType({
   name: "ProcessedSpeech",
   fields: () => ({
     id: {type: GraphQLID}, // cellphone
     fileName: {type: GraphQLString},
+    employeeId: {type: GraphQLString},
+    categorizedSpeechTopic: {type: GraphQLString},
+    speechDuration: {type: GraphQLFloat},
+    silenceDuration: {type: GraphQLFloat},
+    speaker1TalkDuration: {type: GraphQLFloat},
+    speaker2TalkDuration: {type: GraphQLFloat},
+    categorizeResult: {type: new GraphQLList(CategorizedTranscriptType)},
+    
     totalEmoScore: {type: GraphQLFloat},
     totalToneScore: {type: GraphQLFloat},
-    duration: {type: GraphQLFloat},
     abnormalEmotions: {
       type: new GraphQLList(EmotionType)
     },
